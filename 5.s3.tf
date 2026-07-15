c68cd93ab2940670b97c6eff13b4af4b686acd3f
Rayudu <Rayudu@gmail.com>
2026-07-15 18:54:34 +0530

    class-2

--- 5.s3.tf
+++ 5.s3.tf
@@ -9,11 +9,20 @@ resource "aws_s3_bucket" "bhavanibuckets" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "demo" {
+  count = 3
+  bucket              = element(aws_s3_bucket.bhavanibuckets[*].id,count.index)
+  block_public_acls   = false
+  block_public_policy = false    # ← this must be false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
+}
+
 
 resource "aws_s3_bucket_policy" "demo_allow_all" {
   count = 3
   bucket = "bhavanibuckets00${count.index + 1}"
-  #depends_on = [aws_s3_bucket_public_access_block.demo]
+  depends_on = [aws_s3_bucket_public_access_block.demo]
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
