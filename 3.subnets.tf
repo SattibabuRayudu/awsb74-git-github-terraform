07374d996c054f38fed3b9bd704a41caf117a392
Rayudu <Rayudu@gmail.com>
2026-07-15 11:11:39 +0530

    class-2

--- 3.subnets.tf
+++ 3.subnets.tf
@@ -10,6 +10,12 @@ variable "public_subnet_cidrs" {
   #                  0              1               2
 }
 
+variable "private_subnet_cidrs" {
+  type    = list(any)
+  default = ["10.125.10.0/24", "10.125.20.0/24", "10.125.30.0/24"]
+  #                  0              1               2
+}
+
 resource "aws_subnet" "awsb74_vpc_1_public_subnets" {
   count                   = 3 # 0 1 2 
   vpc_id                  = aws_vpc.awsb74_vpc_1.id
@@ -18,6 +24,17 @@ resource "aws_subnet" "awsb74_vpc_1_public_subnets" {
   map_public_ip_on_launch = true
   tags = {
     Name = "awsb74_vpc_1_public_subnet_${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "awsb74_vpc_1_private_subnets" {
+  count                   = 3 # 0 1 2 
+  vpc_id                  = aws_vpc.awsb74_vpc_1.id
+  availability_zone       = element(var.azs, count.index)
+  cidr_block              = element(var.private_subnet_cidrs, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "awsb74_vpc_1_private_subnet_${count.index + 1}"
   }
 }
 
--- 4.routing_table.tf
+++ 4.routing_table.tf
@@ -9,8 +9,24 @@ resource "aws_route_table" "awsb74_vpc_public_rt" {
   }
 }
 
+resource "aws_route_table" "awsb74_vpc_private_rt" {
+  vpc_id = aws_vpc.awsb74_vpc_1.id
+  tags = {
+    Name = "awsb74_vpc_private_rt"
+  }
+}
+
 resource "aws_route_table_association" "awsb74_vpc_1_public_subnets" {
   count = 3
   subnet_id      = element(aws_subnet.awsb74_vpc_1_public_subnets[*].id, count.index)
   route_table_id = aws_route_table.awsb74_vpc_public_rt.id
 }
+
+
+resource "aws_route_table_association" "awsb74_vpc_1_private_subnets" {
+  count = 3
+  subnet_id      = element(aws_subnet.awsb74_vpc_1_private_subnets[*].id, count.index)
+  route_table_id = aws_route_table.awsb74_vpc_private_rt.id
+}
+
+
