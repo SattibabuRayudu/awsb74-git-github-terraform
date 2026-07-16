efd5b3aea0cfc6a90d5bd3b75274bf8c2e6cb717
Rayudu <Rayudu@gmail.com>
2026-07-16 21:10:04 +0530

    class-3

--- 7.ec2.tf
+++ 7.ec2.tf
@@ -6,7 +6,7 @@ data "aws_ami" "my_ami" {
 
 
 resource "aws_instance" "webservers" {
-  count = 3
+  count = 1
   #ami = var.imagename
   #ami = "ami-0d857ff0f5fc4e03b"
   ami                         = data.aws_ami.my_ami.id
