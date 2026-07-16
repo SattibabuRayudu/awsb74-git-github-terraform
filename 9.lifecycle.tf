7fff2b5a1a48ec4e6f0949c15b8fc8edef28b03a
Rayudu <Rayudu@gmail.com>
2026-07-16 21:18:35 +0530

    class-3

--- 9.lifecycle.tf
+++ 9.lifecycle.tf
@@ -1,11 +1,8 @@
-resource "aws_s3_bucket" "awsb74demo100" {
-  bucket        = "awsb74demo100"
+resource "aws_s3_bucket" "bhavanidemo001" {
+  bucket        = "bhavanidemo001"
   force_destroy = true
   tags = {
-    Name        = "awsb74demo100"
+    Name        = "bhavanidemo001"
     Environment = var.env
-  }
-  lifecycle {
-    create_before_destroy = true
   }
 }
