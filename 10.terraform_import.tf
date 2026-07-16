cd904c08d36b944c700d8e368669929530ac25bd
Rayudu <Rayudu@gmail.com>
2026-07-17 03:49:49 +0530

    class-3

--- 10.terraform_import.tf
+++ 10.terraform_import.tf
@@ -1,19 +1,19 @@
-resource "aws_s3_bucket" "bhavani63data" {
-  bucket        = "bhavani63data"
-  force_destroy = true
-  tags = {
-    Name        = "bhavani63data"
-    Environment = var.env
-  }
-}
-resource "aws_s3_bucket" "bhavani63logs" {
-  bucket        = "bhavani63logs"
-  force_destroy = true
-  tags = {
-    Name        = "bhavani63logs"
-    Environment = var.env
-  }
-}
+# resource "aws_s3_bucket" "bhavani63data" {
+#   bucket        = "bhavani63data"
+#   force_destroy = true
+#   tags = {
+#     Name        = "bhavani63data"
+#     Environment = var.env
+#   }
+# }
+# resource "aws_s3_bucket" "bhavani63logs" {
+#   bucket        = "bhavani63logs"
+#   force_destroy = true
+#   tags = {
+#     Name        = "bhavani63logs"
+#     Environment = var.env
+#   }
+# }
 
 
 #terraform import aws_s3_bucket.bhavani63data bhavani63data
