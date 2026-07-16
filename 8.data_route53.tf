6672ce3b4d4b54f5d6ed2ef79b6d4160136d19eb
Rayudu <Rayudu@gmail.com>
2026-07-17 00:52:03 +0530

    class-3

--- 8.data_route53.tf
+++ 8.data_route53.tf
@@ -3,7 +3,8 @@ data "aws_route53_zone" "awsb74_xyz" {
 }
 
 resource "aws_route53_record" "www" {
-  count   = 3
+  #count   = 3
+  count   = length(aws_instance.webservers[*].public_ip)
   zone_id = data.aws_route53_zone.awsb74_xyz.zone_id
   name    = "app${count.index + 1}.${data.aws_route53_zone.awsb74_xyz.name}"
   type    = "A"
