e9f0cfdd89835d2986aeba29be0f20e348d3da4e
Rayudu <Rayudu@gmail.com>
2026-07-19 15:39:49 +0530

    class-4

--- 21.outputs.tf
+++ 21.outputs.tf
@@ -1,6 +1,6 @@
-output "name" {
-  value = data.aws_ami.my_ami.id
-}
+# output "name" {
+#   value = data.aws_ami.my_ami.id
+# }
 
 output "public_ips_of_servers" {
   value = aws_instance.webservers[*].public_ip
